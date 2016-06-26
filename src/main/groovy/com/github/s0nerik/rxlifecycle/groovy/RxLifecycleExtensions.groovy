e328eda425dfa24f36d3a4e4e143b1fc3c30ad43
sonerik <thesonerik@gmail.com>
2016-06-26 03:40:23 +0300

    Added explicit return statements.

--- src/main/groovy/com/github/s0nerik/rxlifecycle/groovy/RxLifecycleExtensions.groovy
+++ src/main/groovy/com/github/s0nerik/rxlifecycle/groovy/RxLifecycleExtensions.groovy
@@ -8,22 +8,22 @@ import rx.Observable
 @CompileStatic
 class RxLifecycleExtensions {
     static <T> Observable<T> bindToLifecycle(final Observable<T> observable, ActivityLifecycleProvider activity) {
-        observable.compose((Observable.Transformer<T, T>) { activity.bindToLifecycle() })
+        return observable.compose((Observable.Transformer<T, T>) { activity.bindToLifecycle() })
     }
 
     static <T> Observable<T> bindUntilEvent(final Observable<T> observable, ActivityLifecycleProvider activity, ActivityEvent event) {
-        observable.compose((Observable.Transformer<T, T>) { activity.bindUntilEvent(event) })
+        return observable.compose((Observable.Transformer<T, T>) { activity.bindUntilEvent(event) })
     }
 
     static <T> Observable<T> bindToLifecycle(final Observable<T> observable, FragmentLifecycleProvider fragment) {
-        observable.compose((Observable.Transformer<T, T>) { fragment.bindToLifecycle() })
+        return observable.compose((Observable.Transformer<T, T>) { fragment.bindToLifecycle() })
     }
 
     static <T> Observable<T> bindUntilEvent(final Observable<T> observable, FragmentLifecycleProvider fragment, FragmentEvent event) {
-        observable.compose((Observable.Transformer<T, T>) { fragment.bindUntilEvent(event) })
+        return observable.compose((Observable.Transformer<T, T>) { fragment.bindUntilEvent(event) })
     }
 
     static <T> Observable<T> bindToLifecycle(final Observable<T> observable, View view) {
-        observable.compose((Observable.Transformer<T, T>) { RxLifecycle.bindView(view) })
+        return observable.compose((Observable.Transformer<T, T>) { RxLifecycle.bindView(view) })
     }
 }
